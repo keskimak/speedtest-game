854228b2d9093c4adfe3a41c3129fb8634bb889f
Noora Keskimäki <noora.keskimaki@gmail.com>
2022-10-28 13:44:04 +0300

    Simple routing now works from LoginScreen to GameScreen via HomeScreen

---
+++ src/screens/GameScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+export default function GameScreen({ navigation }) {
+
+    return (
+
+        <View
+            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Play the game on this page</Text>
+        </View>
+    );
+};
